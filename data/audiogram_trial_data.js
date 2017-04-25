91c24018335841b64667ad277891dbadae9304d0
Becky Gilbert <beckyannegilbert@gmail.com>
2017-04-25 10:53:49 +0100

    initial commit

---
+++ data/audiogram_trial_data.js
@@ -0,0 +1,208 @@
+var trial_data_all = [{
+        "frequency": "500", 
+        "trial_data": [
+        {
+            "soundNumber": 1,
+            "dbReduction": 0,
+            "fileName": "500.mp3",
+        },
+        {
+            "soundNumber": 2,
+            "dbReduction": 5,
+            "fileName": "500_5.mp3",
+        },
+        {
+            "soundNumber": 3,
+            "dbReduction": 10,
+            "fileName": "500_10.mp3", 	 	
+        },
+        {
+            "soundNumber": 4,
+            "dbReduction": 15,
+            "fileName": "500_15.mp3",
+        },
+        {
+            "soundNumber": 5,
+            "dbReduction": 20,
+            "fileName": "500_20.mp3",
+        },
+        {
+            "soundNumber": 6,
+            "dbReduction": 25,
+            "fileName": "500_25.mp3",
+        },
+        {
+            "soundNumber": 7,
+            "dbReduction": 30,
+            "fileName": "500_30.mp3",
+        },
+        {
+            "soundNumber": 8,
+            "dbReduction": 35,
+            "fileName": "500_35.mp3",
+        },
+        {
+            "soundNumber": 9,
+            "dbReduction": 40,
+            "fileName": "500_40.mp3",
+        },
+        {
+            "soundNumber": 10,
+            "dbReduction": 45,
+            "fileName": "500_45.mp3",
+        },
+        {
+            "soundNumber": 11,
+            "dbReduction": 50,
+            "fileName": "500_50.mp3",
+        },
+        {
+            "soundNumber": 12,
+            "dbReduction": 55,
+            "fileName": "500_55.mp3",
+        },
+        {
+            "soundNumber": 13,
+            "dbReduction": 60,
+            "fileName": "500_60.mp3",
+        },
+        {
+            "soundNumber": 14,
+            "dbReduction": 65,
+            "fileName": "500_65.mp3",
+        },
+        {
+            "soundNumber": 15,
+            "dbReduction": 70,
+            "fileName": "500_70.mp3",
+        },
+        {
+            "soundNumber": 16,
+            "dbReduction": 75,
+            "fileName": "500_75.mp3",
+        },
+        {
+            "soundNumber": 17,
+            "dbReduction": 80,
+            "fileName": "500_80.mp3",
+        },
+        {
+            "soundNumber": 18,
+            "dbReduction": 85,
+            "fileName": "500_85.mp3",
+        },
+        {
+            "soundNumber": 19,
+            "dbReduction": 90,
+            "fileName": "500_90.mp3",
+        },
+        {
+            "soundNumber": 20,
+            "dbReduction": "NA",
+            "fileName": "silence.mp3",
+        }]
+    },
+    {
+        "frequency": "1000", 
+        "trial_data": [
+        {
+            "soundNumber": 1,
+            "dbReduction": 0,
+            "fileName": "1000.mp3",
+        },
+        {
+            "soundNumber": 2,
+            "dbReduction": 5,
+            "fileName": "1000_5.mp3",
+        },
+        {
+            "soundNumber": 3,
+            "dbReduction": 10,
+            "fileName": "1000_10.mp3",       
+        },
+        {
+            "soundNumber": 4,
+            "dbReduction": 15,
+            "fileName": "1000_15.mp3",
+        },
+        {
+            "soundNumber": 5,
+            "dbReduction": 20,
+            "fileName": "1000_20.mp3",
+        },
+        {
+            "soundNumber": 6,
+            "dbReduction": 25,
+            "fileName": "1000_25.mp3",
+        },
+        {
+            "soundNumber": 7,
+            "dbReduction": 30,
+            "fileName": "1000_30.mp3",
+        },
+        {
+            "soundNumber": 8,
+            "dbReduction": 35,
+            "fileName": "1000_35.mp3",
+        },
+        {
+            "soundNumber": 9,
+            "dbReduction": 40,
+            "fileName": "1000_40.mp3",
+        },
+        {
+            "soundNumber": 10,
+            "dbReduction": 45,
+            "fileName": "1000_45.mp3",
+        },
+        {
+            "soundNumber": 11,
+            "dbReduction": 50,
+            "fileName": "1000_50.mp3",
+        },
+        {
+            "soundNumber": 12,
+            "dbReduction": 55,
+            "fileName": "1000_55.mp3",
+        },
+        {
+            "soundNumber": 13,
+            "dbReduction": 60,
+            "fileName": "1000_60.mp3",
+        },
+        {
+            "soundNumber": 14,
+            "dbReduction": 65,
+            "fileName": "1000_65.mp3",
+        },
+        {
+            "soundNumber": 15,
+            "dbReduction": 70,
+            "fileName": "1000_70.mp3",
+        },
+        {
+            "soundNumber": 16,
+            "dbReduction": 75,
+            "fileName": "1000_75.mp3",
+        },
+        {
+            "soundNumber": 17,
+            "dbReduction": 80,
+            "fileName": "1000_80.mp3",
+        },
+        {
+            "soundNumber": 18,
+            "dbReduction": 85,
+            "fileName": "1000_85.mp3",
+        },
+        {
+            "soundNumber": 19,
+            "dbReduction": 90,
+            "fileName": "1000_90.mp3",
+        },
+        {
+            "soundNumber": 20,
+            "dbReduction": "NA",
+            "fileName": "silence.mp3",
+        }]
+    }];
