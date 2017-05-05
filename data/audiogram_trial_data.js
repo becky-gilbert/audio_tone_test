4ea0555ccaa701218c94aab9d37fbdd06433e987
Becky Gilbert <beckyannegilbert@gmail.com>
2017-05-05 16:11:29 +0100

    new wav files, 4 frequencies, RMS matched

--- data/audiogram_trial_data.js
+++ data/audiogram_trial_data.js
@@ -3,323 +3,322 @@ var trial_data_all = [{
     "trial_data": [{
         "soundNumber": 1,
         "dbReduction": "NA",
-        "fileName": "silence.mp3"
+        "fileName": "silence.wav"
     }]
 }, {
     "frequency": "500",
     "trial_data": [{
         "soundNumber": 0,
         "dbReduction": 0,
-        "fileName": "500.mp3",
+        "fileName": "500_rmsp527.wav",
     }, {
         "soundNumber": 1,
         "dbReduction": 5,
-        "fileName": "500_5.mp3",
+        "fileName": "500_rmsp527_5.wav",
     }, {
         "soundNumber": 2,
         "dbReduction": 10,
-        "fileName": "500_10.mp3",
+        "fileName": "500_rmsp527_10.wav",
     }, {
         "soundNumber": 3,
         "dbReduction": 15,
-        "fileName": "500_15.mp3",
+        "fileName": "500_rmsp527_15.wav",
     }, {
         "soundNumber": 4,
         "dbReduction": 20,
-        "fileName": "500_20.mp3",
+        "fileName": "500_rmsp527_20.wav",
     }, {
         "soundNumber": 5,
         "dbReduction": 25,
-        "fileName": "500_25.mp3",
+        "fileName": "500_rmsp527_25.wav",
     }, {
         "soundNumber": 6,
         "dbReduction": 30,
-        "fileName": "500_30.mp3",
+        "fileName": "500_rmsp527_30.wav",
     }, {
         "soundNumber": 7,
         "dbReduction": 35,
-        "fileName": "500_35.mp3",
+        "fileName": "500_rmsp527_35.wav",
     }, {
         "soundNumber": 8,
         "dbReduction": 40,
-        "fileName": "500_40.mp3",
+        "fileName": "500_rmsp527_40.wav",
     }, {
         "soundNumber": 9,
         "dbReduction": 45,
-        "fileName": "500_45.mp3",
+        "fileName": "500_rmsp527_45.wav",
     }, {
         "soundNumber": 10,
         "dbReduction": 50,
-        "fileName": "500_50.mp3",
+        "fileName": "500_rmsp527_50.wav",
     }, {
         "soundNumber": 11,
         "dbReduction": 55,
-        "fileName": "500_55.mp3",
+        "fileName": "500_rmsp527_55.wav",
     }, {
         "soundNumber": 12,
         "dbReduction": 60,
-        "fileName": "500_60.mp3",
+        "fileName": "500_rmsp527_60.wav",
     }, {
         "soundNumber": 13,
         "dbReduction": 65,
-        "fileName": "500_65.mp3",
+        "fileName": "500_rmsp527_65.wav",
     }, {
         "soundNumber": 14,
         "dbReduction": 70,
-        "fileName": "500_70.mp3",
+        "fileName": "500_rmsp527_70.wav",
     }, {
         "soundNumber": 15,
         "dbReduction": 75,
-        "fileName": "500_75.mp3",
+        "fileName": "500_rmsp527_75.wav",
     }, {
         "soundNumber": 16,
         "dbReduction": 80,
-        "fileName": "500_80.mp3",
+        "fileName": "500_rmsp527_80.wav",
     }, {
         "soundNumber": 17,
         "dbReduction": 85,
-        "fileName": "500_85.mp3",
+        "fileName": "500_rmsp527_85.wav",
     }, {
         "soundNumber": 18,
         "dbReduction": 90,
-        "fileName": "500_90.mp3",
+        "fileName": "500_rmsp527_90.wav",
     }]
 }, {
     "frequency": "1000",
     "trial_data": [{
         "soundNumber": 0,
         "dbReduction": 0,
-        "fileName": "1000.mp3",
+        "fileName": "1000_rmsp527.wav",
     }, {
         "soundNumber": 1,
         "dbReduction": 5,
-        "fileName": "1000_5.mp3",
+        "fileName": "1000_rmsp527_5.wav",
     }, {
         "soundNumber": 2,
         "dbReduction": 10,
-        "fileName": "1000_10.mp3",
+        "fileName": "1000_rmsp527_10.wav",
     }, {
         "soundNumber": 3,
         "dbReduction": 15,
-        "fileName": "1000_15.mp3",
+        "fileName": "1000_rmsp527_15.wav",
     }, {
         "soundNumber": 4,
         "dbReduction": 20,
-        "fileName": "1000_20.mp3",
+        "fileName": "1000_rmsp527_20.wav",
     }, {
         "soundNumber": 5,
         "dbReduction": 25,
-        "fileName": "1000_25.mp3",
+        "fileName": "1000_rmsp527_25.wav",
     }, {
         "soundNumber": 6,
         "dbReduction": 30,
-        "fileName": "1000_30.mp3",
+        "fileName": "1000_rmsp527_30.wav",
     }, {
         "soundNumber": 7,
         "dbReduction": 35,
-        "fileName": "1000_35.mp3",
+        "fileName": "1000_rmsp527_35.wav",
     }, {
         "soundNumber": 8,
         "dbReduction": 40,
-        "fileName": "1000_40.mp3",
+        "fileName": "1000_rmsp527_40.wav",
     }, {
         "soundNumber": 9,
         "dbReduction": 45,
-        "fileName": "1000_45.mp3",
+        "fileName": "1000_rmsp527_45.wav",
     }, {
         "soundNumber": 10,
         "dbReduction": 50,
-        "fileName": "1000_50.mp3",
+        "fileName": "1000_rmsp527_50.wav",
     }, {
         "soundNumber": 11,
         "dbReduction": 55,
-        "fileName": "1000_55.mp3",
+        "fileName": "1000_rmsp527_55.wav",
     }, {
         "soundNumber": 12,
         "dbReduction": 60,
-        "fileName": "1000_60.mp3",
+        "fileName": "1000_rmsp527_60.wav",
     }, {
         "soundNumber": 13,
         "dbReduction": 65,
-        "fileName": "1000_65.mp3",
+        "fileName": "1000_rmsp527_65.wav",
     }, {
         "soundNumber": 14,
         "dbReduction": 70,
-        "fileName": "1000_70.mp3",
+        "fileName": "1000_rmsp527_70.wav",
     }, {
         "soundNumber": 15,
         "dbReduction": 75,
-        "fileName": "1000_75.mp3",
+        "fileName": "1000_rmsp527_75.wav",
     }, {
         "soundNumber": 16,
         "dbReduction": 80,
-        "fileName": "1000_80.mp3",
+        "fileName": "1000_rmsp527_80.wav",
     }, {
         "soundNumber": 17,
         "dbReduction": 85,
-        "fileName": "1000_85.mp3",
+        "fileName": "1000_rmsp527_85.wav",
     }, {
         "soundNumber": 18,
         "dbReduction": 90,
-        "fileName": "1000_90.mp3",
+        "fileName": "1000_rmsp527_90.wav",
+    }]
+}, {
+    "frequency": "2000",
+    "trial_data": [{
+        "soundNumber": 0,
+        "dbReduction": 0,
+        "fileName": "2000_rmsp527.wav",
+    }, {
+        "soundNumber": 1,
+        "dbReduction": 5,
+        "fileName": "2000_rmsp527_5.wav",
+    }, {
+        "soundNumber": 2,
+        "dbReduction": 10,
+        "fileName": "2000_rmsp527_10.wav",
+    }, {
+        "soundNumber": 3,
+        "dbReduction": 15,
+        "fileName": "2000_rmsp527_15.wav",
+    }, {
+        "soundNumber": 4,
+        "dbReduction": 20,
+        "fileName": "2000_rmsp527_20.wav",
+    }, {
+        "soundNumber": 5,
+        "dbReduction": 25,
+        "fileName": "2000_rmsp527_25.wav",
+    }, {
+        "soundNumber": 6,
+        "dbReduction": 30,
+        "fileName": "2000_rmsp527_30.wav",
+    }, {
+        "soundNumber": 7,
+        "dbReduction": 35,
+        "fileName": "2000_rmsp527_35.wav",
+    }, {
+        "soundNumber": 8,
+        "dbReduction": 40,
+        "fileName": "2000_rmsp527_40.wav",
+    }, {
+        "soundNumber": 9,
+        "dbReduction": 45,
+        "fileName": "2000_rmsp527_45.wav",
+    }, {
+        "soundNumber": 10,
+        "dbReduction": 50,
+        "fileName": "2000_rmsp527_50.wav",
+    }, {
+        "soundNumber": 11,
+        "dbReduction": 55,
+        "fileName": "2000_rmsp527_55.wav",
+    }, {
+        "soundNumber": 12,
+        "dbReduction": 60,
+        "fileName": "2000_rmsp527_60.wav",
+    }, {
+        "soundNumber": 13,
+        "dbReduction": 65,
+        "fileName": "2000_rmsp527_65.wav",
+    }, {
+        "soundNumber": 14,
+        "dbReduction": 70,
+        "fileName": "2000_rmsp527_70.wav",
+    }, {
+        "soundNumber": 15,
+        "dbReduction": 75,
+        "fileName": "2000_rmsp527_75.wav",
+    }, {
+        "soundNumber": 16,
+        "dbReduction": 80,
+        "fileName": "2000_rmsp527_80.wav",
+    }, {
+        "soundNumber": 17,
+        "dbReduction": 85,
+        "fileName": "2000_rmsp527_85.wav",
+    }, {
+        "soundNumber": 18,
+        "dbReduction": 90,
+        "fileName": "2000_rmsp527_90.wav",
+    }]
+}, {
+    "frequency": "4000",
+    "trial_data": [{
+        "soundNumber": 0,
+        "dbReduction": 0,
+        "fileName": "4000_rmsp527.wav",
+    }, {
+        "soundNumber": 1,
+        "dbReduction": 5,
+        "fileName": "4000_rmsp527_5.wav",
+    }, {
+        "soundNumber": 2,
+        "dbReduction": 10,
+        "fileName": "4000_rmsp527_10.wav",
+    }, {
+        "soundNumber": 3,
+        "dbReduction": 15,
+        "fileName": "4000_rmsp527_15.wav",
+    }, {
+        "soundNumber": 4,
+        "dbReduction": 20,
+        "fileName": "4000_rmsp527_20.wav",
+    }, {
+        "soundNumber": 5,
+        "dbReduction": 25,
+        "fileName": "4000_rmsp527_25.wav",
+    }, {
+        "soundNumber": 6,
+        "dbReduction": 30,
+        "fileName": "4000_rmsp527_30.wav",
+    }, {
+        "soundNumber": 7,
+        "dbReduction": 35,
+        "fileName": "4000_rmsp527_35.wav",
+    }, {
+        "soundNumber": 8,
+        "dbReduction": 40,
+        "fileName": "4000_rmsp527_40.wav",
+    }, {
+        "soundNumber": 9,
+        "dbReduction": 45,
+        "fileName": "4000_rmsp527_45.wav",
+    }, {
+        "soundNumber": 10,
+        "dbReduction": 50,
+        "fileName": "4000_rmsp527_50.wav",
+    }, {
+        "soundNumber": 11,
+        "dbReduction": 55,
+        "fileName": "4000_rmsp527_55.wav",
+    }, {
+        "soundNumber": 12,
+        "dbReduction": 60,
+        "fileName": "4000_rmsp527_60.wav",
+    }, {
+        "soundNumber": 13,
+        "dbReduction": 65,
+        "fileName": "4000_rmsp527_65.wav",
+    }, {
+        "soundNumber": 14,
+        "dbReduction": 70,
+        "fileName": "4000_rmsp527_70.wav",
+    }, {
+        "soundNumber": 15,
+        "dbReduction": 75,
+        "fileName": "4000_rmsp527_75.wav",
+    }, {
+        "soundNumber": 16,
+        "dbReduction": 80,
+        "fileName": "4000_rmsp527_80.wav",
+    }, {
+        "soundNumber": 17,
+        "dbReduction": 85,
+        "fileName": "4000_rmsp527_85.wav",
+    }, {
+        "soundNumber": 18,
+        "dbReduction": 90,
+        "fileName": "4000_rmsp527_90.wav",
     }]
 }];
-// }, {
-//     "frequency": "2000",
-//     "trial_data": [{
-//         "soundNumber": 0,
-//         "dbReduction": 0,
-//         "fileName": "2000.mp3",
-//     }, {
-//         "soundNumber": 1,
-//         "dbReduction": 5,
-//         "fileName": "2000_5.mp3",
-//     }, {
-//         "soundNumber": 2,
-//         "dbReduction": 10,
-//         "fileName": "2000_10.mp3",
-//     }, {
-//         "soundNumber": 3,
-//         "dbReduction": 15,
-//         "fileName": "2000_15.mp3",
-//     }, {
-//         "soundNumber": 4,
-//         "dbReduction": 20,
-//         "fileName": "2000_20.mp3",
-//     }, {
-//         "soundNumber": 5,
-//         "dbReduction": 25,
-//         "fileName": "2000_25.mp3",
-//     }, {
-//         "soundNumber": 6,
-//         "dbReduction": 30,
-//         "fileName": "2000_30.mp3",
-//     }, {
-//         "soundNumber": 7,
-//         "dbReduction": 35,
-//         "fileName": "2000_35.mp3",
-//     }, {
-//         "soundNumber": 8,
-//         "dbReduction": 40,
-//         "fileName": "2000_40.mp3",
-//     }, {
-//         "soundNumber": 9,
-//         "dbReduction": 45,
-//         "fileName": "2000_45.mp3",
-//     }, {
-//         "soundNumber": 10,
-//         "dbReduction": 50,
-//         "fileName": "2000_50.mp3",
-//     }, {
-//         "soundNumber": 11,
-//         "dbReduction": 55,
-//         "fileName": "2000_55.mp3",
-//     }, {
-//         "soundNumber": 12,
-//         "dbReduction": 60,
-//         "fileName": "2000_60.mp3",
-//     }, {
-//         "soundNumber": 13,
-//         "dbReduction": 65,
-//         "fileName": "2000_65.mp3",
-//     }, {
-//         "soundNumber": 14,
-//         "dbReduction": 70,
-//         "fileName": "2000_70.mp3",
-//     }, {
-//         "soundNumber": 15,
-//         "dbReduction": 75,
-//         "fileName": "2000_75.mp3",
-//     }, {
-//         "soundNumber": 16,
-//         "dbReduction": 80,
-//         "fileName": "2000_80.mp3",
-//     }, {
-//         "soundNumber": 17,
-//         "dbReduction": 85,
-//         "fileName": "2000_85.mp3",
-//     }, {
-//         "soundNumber": 18,
-//         "dbReduction": 90,
-//         "fileName": "2000_90.mp3",
-//     }]
-// }, {
-//     "frequency": "4000",
-//     "trial_data": [{
-//         "soundNumber": 0,
-//         "dbReduction": 0,
-//         "fileName": "4000.mp3",
-//     }, {
-//         "soundNumber": 1,
-//         "dbReduction": 5,
-//         "fileName": "4000_5.mp3",
-//     }, {
-//         "soundNumber": 2,
-//         "dbReduction": 10,
-//         "fileName": "4000_10.mp3",
-//     }, {
-//         "soundNumber": 3,
-//         "dbReduction": 15,
-//         "fileName": "4000_15.mp3",
-//     }, {
-//         "soundNumber": 4,
-//         "dbReduction": 20,
-//         "fileName": "4000_20.mp3",
-//     }, {
-//         "soundNumber": 5,
-//         "dbReduction": 25,
-//         "fileName": "4000_25.mp3",
-//     }, {
-//         "soundNumber": 6,
-//         "dbReduction": 30,
-//         "fileName": "4000_30.mp3",
-//     }, {
-//         "soundNumber": 7,
-//         "dbReduction": 35,
-//         "fileName": "4000_35.mp3",
-//     }, {
-//         "soundNumber": 8,
-//         "dbReduction": 40,
-//         "fileName": "4000_40.mp3",
-//     }, {
-//         "soundNumber": 9,
-//         "dbReduction": 45,
-//         "fileName": "4000_45.mp3",
-//     }, {
-//         "soundNumber": 10,
-//         "dbReduction": 50,
-//         "fileName": "4000_50.mp3",
-//     }, {
-//         "soundNumber": 11,
-//         "dbReduction": 55,
-//         "fileName": "4000_55.mp3",
-//     }, {
-//         "soundNumber": 12,
-//         "dbReduction": 60,
-//         "fileName": "4000_60.mp3",
-//     }, {
-//         "soundNumber": 13,
-//         "dbReduction": 65,
-//         "fileName": "4000_65.mp3",
-//     }, {
-//         "soundNumber": 14,
-//         "dbReduction": 70,
-//         "fileName": "4000_70.mp3",
-//     }, {
-//         "soundNumber": 15,
-//         "dbReduction": 75,
-//         "fileName": "4000_75.mp3",
-//     }, {
-//         "soundNumber": 16,
-//         "dbReduction": 80,
-//         "fileName": "4000_80.mp3",
-//     }, {
-//         "soundNumber": 17,
-//         "dbReduction": 85,
-//         "fileName": "4000_85.mp3",
-//     }, {
-//         "soundNumber": 18,
-//         "dbReduction": 90,
-//         "fileName": "4000_90.mp3",
-//     }]
-// }];
